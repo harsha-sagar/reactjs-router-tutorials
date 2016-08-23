af3401cf87e6108ec08b7c8851097ba868adfd5d
km-sagar <harsha@liftoffllc.com>
2016-08-23 19:22:51 +0530

    example showing nested route passing multiple child components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router';
 
-const Outer = (props) => <div><h1>Our Site</h1><Links />{props.children}</div>;
-const About = (props) => <div><h1>About</h1>{props.children}</div>;
-const Contact = (props) => <div><h1>Contact</h1>{props.children}</div>;
+const Home = () => <h1>home</h1>;
+const HomeBody = () => <div>this is home body</div>;
+const Other = () => <h1>other</h1>;
+const OtherBody = () => <div>this is other body</div>;
+
+const Container = (props) => <div>{props.header}{props.body}<br/><Links/></div>;
 
 const Links = () =>
     <nav>
-        <Link to="/">Home</Link><br/>
-        <Link to="/about">About</Link><br/>
-        <Link to="/contact">Contact</Link><br/>
+        <Link to="/">home</Link><br/>
+        <Link to="/other">other</Link><br/>
     </nav>
 
 class App extends React.Component {
     render(){
         return (
             <Router history={ hashHistory }>
-                <Route path="/" component={Outer}>
-                    <IndexRoute component={About}></IndexRoute>
-                    <Route path="about" component={About}></Route>
-                    <Route path="contact" component={Contact}></Route>
+                <Route path="/" component={Container}>
+                    <IndexRoute components={{header: Home, body: HomeBody}}></IndexRoute>
+                    <Route path="other" components={{header: Other, body: OtherBody}}></Route>
                 </Route>
             </Router>
         );
