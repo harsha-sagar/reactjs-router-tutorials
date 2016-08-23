1ab8412f0b5241622794d48aaed1fac09109bd5e
km-sagar <harsha@liftoffllc.com>
2016-08-23 16:08:37 +0530

    intro to Router component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Router, Route, Link, hashHistory } from 'react-router';
 
-const App = () => {
-    return <h1>hello world</h1>
+const Home = () => <div><h1>Home</h1><Links /></div>;
+const About = () => <div><h1>About</h1><Links /></div>;
+const Contact = () => <div><h1>Contact</h1><Links /></div>;
+
+const Links = () =>
+    <nav>
+        <Link to="/">Home</Link><br/>
+        <Link to="/about">About</Link><br/>
+        <Link to="/contact">Contact</Link><br/>
+    </nav>
+
+class App extends React.Component {
+    render(){
+        return (
+            <Router history={ hashHistory }>
+                <Route path="/" component={Home}></Route>
+                <Route path="/about" component={About}></Route>
+                <Route path="/contact" component={Contact}></Route>
+            </Router>
+        );
+    }
 }
 
 export default App;
