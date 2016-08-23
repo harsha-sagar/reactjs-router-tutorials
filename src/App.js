a3a870f61e30d219caa184d3b68e36dbb1d716dd
km-sagar <harsha@liftoffllc.com>
2016-08-23 17:16:42 +0530

    understanding nested routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import { Router, Route, Link, hashHistory } from 'react-router';
 
-const Home = () => <div><h1>Home</h1><Links /></div>;
-const About = () => <div><h1>About</h1><Links /></div>;
-const Contact = () => <div><h1>Contact</h1><Links /></div>;
+const Home = (props) => <div><h1>Home</h1><Links />{props.children}</div>;
+const About = (props) => <div><h1>About</h1>{props.children}</div>;
+const Contact = (props) => <div><h1>Contact</h1>{props.children}</div>;
 
 const Links = () =>
     <nav>
         <Link to="/">Home</Link><br/>
         <Link to="/about">About</Link><br/>
-        <Link to="/contact">Contact</Link><br/>
+        <Link to="/about/contact">Contact</Link><br/>
     </nav>
 
 class App extends React.Component {
     render(){
         return (
             <Router history={ hashHistory }>
-                <Route path="/" component={Home}></Route>
-                <Route path="/about" component={About}></Route>
-                <Route path="/contact" component={Contact}></Route>
+                <Route path="/" component={Home}>
+                    <Route path="about" component={About}>
+                        <Route path="contact" component={Contact}></Route>
+                    </Route>
+                </Route>
             </Router>
         );
     }
 }
+
+/*
+    On clicking "/" link, Home component gets rendered.
+    On clicking "/about" link, Home component gets rendered. Plus About component is passed as children to Home component
+    On clicking "/about/contact" link -
+    a. Home component gets rendered.
+    b. About component is intiated & passed as children to Home component
+    c. Contact component is intiated & passed as children to About component
+*/
 
 export default App;
